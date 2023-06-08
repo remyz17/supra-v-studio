73ef2e1e6da316e3c8df7556cf0483bb3e36067b
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-08 10:11:11 +0200

    feat(App.vue): add header and footer components to the app layout to improve user experience and navigation. Add a fade transition to the router view to improve visual feedback.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import VTransitionFade from '@/components/VTransitionFade.vue'
 import { RouterView } from 'vue-router'
+import TheFooter from './layouts/TheFooter.vue'
+import TheHeader from './layouts/TheHeader.vue'
 </script>
 
 <template>
-  <RouterView />
+  <TheHeader />
+  <div class="shadow-sm flex-grow z-10 bg-black">
+    <RouterView v-slot="{ Component }">
+      <VTransitionFade>
+        <component :is="Component" />
+      </VTransitionFade>
+    </RouterView>
+  </div>
+  <TheFooter />
 </template>
